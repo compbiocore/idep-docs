da4f9b74a87f75ae3ebad26a3e56714a6b04f778
andrew-leith <andrew-leith@users.noreply.github.com>
2019-08-13 16:04:52 -0400

    updated docker run command

--- run_idep_with_webapp.sh
+++ run_idep_with_webapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --expose 3838 -p 80:3838 compbiocore/idep-with-webapp:latest /bin/bash
+docker run -it --expose 3838 -p 80:3838 compbiocore/idep-with-webapp:latest /usr/bin/shiny-server.sh
